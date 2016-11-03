f1ca51d9fa768dcc6659230eb5eb9fe3765e7f55
Fortune <fortuneochi@gmail.com>
2016-11-03 20:47:57 +0000

    Time stamp microservice

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-var express = require("express");
-var app = express();
+var app = require("express")();
 
-app.get('/',(req,res)=>{
-   res.send("Hello World!");
+
+app.get('/:arg1', (req, res) => {
+   var input = req.params.arg1;
+   var date, unix;
+   if (/^\d+$/.test(input)) date = new Date(1000 * input);
+   else date = new Date(input);
+
+   unix = date.getTime();
+   date = date.toDateString();
+   date = date == "Invalid Date" ? null : date;
+   res.send({
+      unix: unix / 1000,
+      natural: date
+   });
 });
-app.listen(8080,()=>{
+app.listen(8080, () => {
    console.log("Listening on port 8080");
 });
